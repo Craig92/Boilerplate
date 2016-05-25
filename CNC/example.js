0c76d6825fd90779cf66240c73459a669029ce4b
Thorsten <thorsten@familie-foehringer.de>
2016-05-25 15:30:43 +0200

    Verschiebung des Examples und Änderungen an der Example

--- CNC/example.js
+++ CNC/example.js
@@ -16,3 +16,19 @@ app.get('/api/Tasks', (req, res) => {
 app.get('/api/Tasks/:id', (req, res) =>{
     res.send('task id was' + req.params.id)
 });
+
+var express = require('express');
+var app     = express();
+var parser  = require('body-parser');
+
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+// Other middlewares and routes
+
+app.post('/Tasks/:id', (req, res) => {
+    console.log('Received data', req.body);
+    res.json({ message: 'UPDATE Task ' + req.params.id });
+});
+
+app.listen(3000);
