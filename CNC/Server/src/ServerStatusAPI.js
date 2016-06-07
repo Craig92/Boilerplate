a3cd3b5938c88c55db504a39b31405fcf129372e
alexdaum <alexander-daum@t-online.de>
2016-06-07 17:44:16 +0200

    serverStatusAPI    .get (geht vielleicht?!?)

--- CNC/Server/src/ServerStatusAPI.js
+++ CNC/Server/src/ServerStatusAPI.js
@@ -1,24 +1,72 @@
-var express = require('express');
+const express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var app = express();
+const app = express();
 var serverPort = 1337;
 
 //STATUS GET REQUEST
 app.get('/api/Status', (req, res) => {
-    @Julian & Alexander
-    liefert Arra mit allen Status einträgen
-    //TODO Request liefert Array an Bot Status Objekten
-    //liest aus ServerStatus.txt
+    var fs = require('fs');
+    var statusArray = [];
+
+        fs.readFile('ServerStatus.txt', 'utf8', (err, data) => {
+            if (err) throw err;
+            var statusString = data.toString('utf8');
+            var indexKlammerAuf = [];
+            var indexKlammerZu = [];
+            
+            for(var i = 0; i < statusString.length; i++){
+                if(statusString.charAt(i) == '{'){
+                    indexKlammerAuf.push(i);
+                } else if(statusString.charAt(i) == '}'){
+                    indexKlammerZu.push(i);  
+                }                      
+            }
+
+            for(var i = 0; i < indexKlammerAuf.length; i++){
+                statusArray.push(statusString.slice(indexKlammerAuf[i],indexKlammerZu[i]));
+            }
+
+        });    
+
+        res.send(JSON.stringify(statusArray));
     });
 
 app.get('/api/Status/:id', (req, res) => {
 
-    liefert objekt der übergebenen id
-    gibt fehlermedlung wenn id nicht vorhanden
-    //TODO Request liefert Array an Bot Status Objekten
-    //liest aus ServerStatus.txt
+  var fs = require('fs');
+    var statusArray = [];
+
+        fs.readFile('ServerStatus.txt', 'utf8', (err, data) => {
+            if (err) throw err;
+            var statusString = data.toString('utf8');
+            var indexKlammerAuf = [];
+            var indexKlammerZu = [];
+            
+            for(var i = 0; i < statusString.length; i++){
+                if(statusString.charAt(i) == '{'){
+                    indexKlammerAuf.push(i);
+                } else if(statusString.charAt(i) == '}'){
+                    indexKlammerZu.push(i);  
+                }                      
+            }
+
+            for(var i = 0; i < indexKlammerAuf.length; i++){
+                statusArray.push(statusString.slice(indexKlammerAuf[i],indexKlammerZu[i]));
+            }
+
+            for(var I =0; i < statusArray.length; i++){
+               if(statusArray[i].search("\"id\": " + req.params.id) != -1) {
+                   res.send(JSON.stringify(statusArray[i]));
+               }
+
+                
+            }
+
+        });    
+
     });
+
     
 //Status POST REQUEST
 app.post('/api/Status', (req, res) => {
