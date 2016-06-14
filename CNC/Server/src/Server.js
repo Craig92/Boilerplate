3611c2bab1f6671bd75fef42eabab003b81911cc
Thorsten <thorsten@familie-foehringer.de>
2016-06-14 11:21:02 +0200

    änderungen

--- CNC/Server/src/Server.js
+++ CNC/Server/src/Server.js
@@ -218,7 +218,7 @@ app.post('/api/Tasks', (req, res) => {
                 req.body = counter;
                 req.body.data.output = 'null';
                 counter++;
-                tasksArray.push(req);
+                tasksArray.push(tempTask);
 
             }
             /*
